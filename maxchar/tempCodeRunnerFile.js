8d46e2962089d6eca27c7c0627ef9f2a52ff959d
Ori Nizri Tal <ori.nizri.tal@gmail.com>
2022-03-30 17:56:10 +0300

    max char

--- maxchar/tempCodeRunnerFile.js
+++
@@ -1,5 +0,0 @@
-    // const highestVal = Math.max.apply(null, Object.values(dict)),
-    // val = Object.keys(dict).find(function(a) {
-    //     return dict[a] === highestVal;
-    // });
-    // return val
