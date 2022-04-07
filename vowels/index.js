48126091bf25943270f75c840e141155414fa63f
Ori Nizri Tal <ori.nizri.tal@gmail.com>
2022-04-07 12:40:12 +0300

    vowels plus regex

--- vowels/index.js
+++ vowels/index.js
@@ -10,12 +10,12 @@
 function vowels(str) {
     let vowelsCounter = 0;
     const vowels = ['a', 'e', 'i', 'o', 'u'];
-    for (let char of str) {
-        if (vowels.includes(char.toLowerCase())) {
+    for (let char of str.toLowerCase()) {
+        if (vowels.includes(char)) {
             vowelsCounter ++;
         }
     }
-    return vowelsCounter
+    return vowelsCounter;
 }
 
 function vowels2(str, counter=0) {
@@ -24,9 +24,14 @@ function vowels2(str, counter=0) {
         if (vowels.includes(str[str.length - 1].toLowerCase())) {
             counter ++;
         }
-    str = str.slice(0,-1)
-    return vowels2(str, counter)
+    str = str.slice(0,-1);
+    return vowels2(str, counter);
 }
 
-console.log(vowels2('Hi There'));
+function vowels3(str, counter=0) {
+    const matches = str.match(/[aeiou]/gi)
+    return matches ? matches.length : 0;
+}
+
+console.log(vowels3('Hi There'));
 module.exports = vowels;
