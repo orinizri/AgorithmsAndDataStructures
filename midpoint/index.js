91f3b85dc76fa96db96c99422ae4d949f34377bd
Ori Nizri Tal <ori.nizri.tal@gmail.com>
2022-04-19 21:40:31 +0300

    refactor

--- midpoint/index.js
+++ midpoint/index.js
@@ -24,22 +24,18 @@ function midpoint(list) {
     }
     return slowNode;
 }
-
 function midpoint2(list) {
     let slow = list.getFirst();
     let fast = list.getFirst();
-
     while (fast.next && fast.next.next) {
         slow = slow.next;
         fast = fast.next.next;
     }
     return slow;
 }
-
   const l = new LinkedList();
   l.insertLast('a')
   l.insertLast('b')
   l.insertLast('c')
 console.log(midpoint2(l)); // returns { data: 'b' }
-
 module.exports = midpoint;
