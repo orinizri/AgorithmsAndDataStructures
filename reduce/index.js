db2fc18556ef3c29d7fa00f467a00a6e3a44d397
Ori Nizri Tal <ori.nizri.tal@gmail.com>
2022-05-10 19:48:03 +0300

    reduce without reduce function

---
+++ reduce/index.js
@@ -0,0 +1,13 @@
+
+
+
+
+const reduce = (array, cb, initialValue) => {
+    let result = initialValue;
+    array.forEach(item =>
+        result = cb.call(null, result, item));
+    return result;
+};
+
+console.log(reduce([1,2], (a,b)=> a+b,0))
+console.log(reduce(["a","b"], (a,b)=> a+b,''))
