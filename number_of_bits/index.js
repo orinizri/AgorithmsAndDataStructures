6633a74f672807d17968cd6e752546f0a1d5c61f
Ori Nizri Tal <ori.nizri.tal@gmail.com>
2022-05-25 22:33:04 +0300

    number of bits

---
+++ number_of_bits/index.js
@@ -0,0 +1,16 @@
+
+/**
+ * Write a function that takes an unsigned integer 
+ * and returns the number of '1' bits it has (also known as the Hamming weight).
+ * @param {number} n - a positive integer
+ * @return {number}
+ */
+
+function hammingWeight (n) {
+    let num_of_1s = 0;
+    for (let i = 0; i < 32; i++) {
+        num_of_1s += n & 1;
+        n >>= 1;
+    }
+    return num_of_1s
+};
