cf7d96ed04ccfec53ee7f8634e2ea595ee90a8f0
Ori Nizri Tal <ori.nizri.tal@gmail.com>
2022-05-26 20:32:31 +0300

    reduce v2

--- reduce/index.js
+++ reduce/index.js
@@ -1,12 +1,31 @@
 
+// write a function that will mimic reduce behavior
 
+//testing performance 
+var startTime = performance.now()
 
-const reduce = (array, cb, initialValue) => {
-    let result = initialValue;
+// version 1 - better, dropped the fn.call 
+const reduce = (array, fn, initialValue = '') => {
+    let result = initialValue || array.shift();
     array.forEach(item =>
-        result = cb.call(undefined, result, item));
+        result = fn(result, item));
     return result;
 };
 
-console.log(reduce([1,2], (a,b)=> a+b,0))
-console.log(reduce(["a","b"], (a,b)=> a+b,''))
+// version 2 - slower 
+const reduce_v2 = (fn, array, initialValue = 0) => {
+    let result = initialValue || array.shift();
+    while (array.length > 0) {
+        result = fn(result , ...array.splice(0,1))
+    } 
+    return result;
+};
+
+// console.log(reduce([1,2,3], (a,b)=> a+b,0))
+// console.log(reduce(["a","b","c"], (a,b)=> a+b))
+console.log(reduce_v2((a,b)=> a+b, [1,2,3], 0))
+console.log(reduce_v2((a,b)=> a+b, ["a","b","c"]))
+
+var endTime = performance.now()
+
+console.log(`Call to doSomething took ${endTime - startTime} milliseconds`)
