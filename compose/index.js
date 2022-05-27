dd42933069cd58efca62c598a6040ea0fabfa8e6
Ori Nizri Tal <ori.nizri.tal@gmail.com>
2022-05-27 22:16:19 +0300

    compose

--- compose/index.js
+++ compose/index.js
@@ -1,11 +1,12 @@
 
 
-const compose = (f, g) => (initialValue) => f(g(initialValue));
+const compose = (func1, func2) => (initialValue) => func1(func2(initialValue));
 
 const addOne = (num) => Number(num) + 1;
 
 console.log(compose(addOne, addOne)(5))
 
-const compose2 = (...fns) => (...args) => fns.reduceRight((acc, fn) => fn(acc), args);
+const compose2 = (...fns) => (...args) => 
+    fns.reduceRight((acc, fn) => fn(acc), args);
 
 console.log(compose2(addOne, addOne, addOne)(5))
